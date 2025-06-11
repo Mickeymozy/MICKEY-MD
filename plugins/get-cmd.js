8b6e8c338c3fa6ddee949168d228b7ca27abd6f2
Mickeymozy <157571260+Mickeymozy@users.noreply.github.com>
2025-06-12 00:57:06 +0300

    Update get-cmd.js

--- plugins/get-cmd.js
+++ plugins/get-cmd.js
@@ -43,7 +43,7 @@ ${truncatedCode}
 
         // Send image with truncated source code
         await conn.sendMessage(from, { 
-            image: { url: `https://i.ibb.co/4ZSYvPTq/lordali.jpg` },  // Image URL
+            image: { url: `https://files.catbox.moe/5j6oz3.jpg` },  // Image URL
             caption: formattedCode,
             contextInfo: {
                 mentionedJid: [m.sender],
