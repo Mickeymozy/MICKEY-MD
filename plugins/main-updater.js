8d0505ff8b820132ba5203b31700022eb5bbd960
Mickeymozy <157571260+Mickeymozy@users.noreply.github.com>
2025-06-12 17:55:32 +0300

    Add files via upload

--- plugins/main-updater.js
+++ plugins/main-updater.js
@@ -16,24 +16,24 @@ cmd({
     if (!isOwner) return reply("This command is only for the bot owner.");
 
     try {
-        await reply("🔍 Checking for TOHID_MD updates...");
+        await reply("🔍 Checking for Mickey-MD updates...");
 
         // Fetch the latest commit hash from GitHub
-        const { data: commitData } = await axios.get("https://api.github.com/repos/Tohidkhan6332/TOHID_MD/commits/main");
+        const { data: commitData } = await axios.get("https://api.github.com/repos/Mickeymozy/MICKEY-MD/commits/main");
         const latestCommitHash = commitData.sha;
 
         // Get the stored commit hash from the database
         const currentHash = await getCommitHash();
 
         if (latestCommitHash === currentHash) {
-            return reply("✅ Your TOHID_MD bot is already up-to-date!");
+            return reply("✅ Your bot is already up-to-date!");
         }
 
-        await reply("🚀 Updating TOHID_MD Bot...");
+        await reply("🚀 Updating  Bot...");
 
         // Download the latest code
         const zipPath = path.join(__dirname, "latest.zip");
-        const { data: zipData } = await axios.get("https://github.com/Tohidkhan6332/TOHID_MD/archive/main.zip", { responseType: "arraybuffer" });
+        const { data: zipData } = await axios.get("https://github.com/Mickeymozy/MICKEY-MD/archive/main.zip", { responseType: "arraybuffer" });
         fs.writeFileSync(zipPath, zipData);
 
         // Extract ZIP file
@@ -44,7 +44,7 @@ cmd({
 
         // Copy updated files, preserving config.js and app.json
         await reply("🔄 Replacing files...");
-        const sourcePath = path.join(extractPath, "TOHID_MD-main");
+        const sourcePath = path.join(extractPath, "MICKEY-MD-main");
         const destinationPath = path.join(__dirname, '..');
         copyFolderSync(sourcePath, destinationPath);
 
--- plugins/ping.js
+++ plugins/ping.js
@@ -32,7 +32,7 @@ async (conn, mek, m, { from, quoted, sender, reply }) => {
         const end = new Date().getTime();
         const responseTime = (end - start) / 1000;
 
-        const text = `> *𝐓𝐎𝐇𝐈𝐃_𝐌𝐃 𝐒𝐏𝐄𝐄𝐃: ${responseTime.toFixed(2)}ms ${reactionEmoji}*`;
+        const text = `> *𝕄𝕀ℂ𝕂𝔼𝕐-𝕄𝔻 𝕊ℙ𝔼𝔼𝔻: ${responseTime.toFixed(2)}ms ${reactionEmoji}*`;
 
         await conn.sendMessage(from, {
             text,
@@ -41,8 +41,8 @@ async (conn, mek, m, { from, quoted, sender, reply }) => {
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363207624903731@newsletter',
-                    newsletterName: "𝐓𝐎𝐇𝐈𝐃 𝐓𝐄𝐂𝐇 🤖",
+                    newsletterJid: '12036331487561459@newsletter',
+                    newsletterName: "𝙼𝙸𝙲𝙺𝙴𝚈-𝙼𝙳",
                     serverMessageId: 143
                 }
             }
@@ -69,7 +69,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const message = await conn.sendMessage(from, { text: '*PINGING...*' })
         const endTime = Date.now()
         const ping = endTime - startTime
-        await conn.sendMessage(from, { text: `*🔥 𝐓𝐎𝐇𝐈𝐃_𝐌𝐃 𝐒𝐏𝐄𝐄𝐃 : ${ping}ms*` }, { quoted: message })
+        await conn.sendMessage(from, { text: `*🔥 𝕄𝕀ℂ𝕂𝔼𝕐-𝕄𝔻 𝕊ℙ𝔼𝔼𝔻 : ${ping}ms*` }, { quoted: message })
     } catch (e) {
         console.log(e)
         reply(`${e}`)
